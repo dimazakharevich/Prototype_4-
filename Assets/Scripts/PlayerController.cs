948053b97a52e09488d3a2f33f7adeb85dbf986f
Dmitry Zakharevich <dima.zakharevich95@gmail.com>
2020-10-24 23:42:34 -0600

    Added Player and spawn enemies. Ability to move. And random spawn.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]float speed;
+    GameObject focalPoint;
+
+    Rigidbody playerRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRigidBody.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    float spawnRange = 9f;
+    [SerializeField] GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, GenerateSpawnPositiob(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 GenerateSpawnPositiob()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
