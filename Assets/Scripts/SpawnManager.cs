cb3be5486a364dad3f3e86425bce0e599b5f0d78
Dmitry Zakharevich <dima.zakharevich95@gmail.com>
2020-10-27 11:54:05 -0600

    Added levels and increasing difficulty

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,24 +6,43 @@ public class SpawnManager : MonoBehaviour
 {
     float spawnRange = 9f;
     [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int enemyCount;
+    [SerializeField] GameObject powerupPrefab;
+    int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPositiob(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPositiob(1), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPositiob(1) ,powerupPrefab.transform.rotation);
+        }
     }
 
-    Vector3 GenerateSpawnPositiob()
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPositiob(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    Vector3 GenerateSpawnPositiob(float y =0)
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, y, spawnPosZ);
 
         return randomPos;
     }
